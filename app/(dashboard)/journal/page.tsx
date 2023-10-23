1dc38d508048d245e34d7c9600c11853dc14c82d
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2023-10-23 14:18:45 -0400

    add Prisma db w/ PlanetScale and Clerk auth

---
+++ app/(dashboard)/journal/page.tsx
@@ -0,0 +1,30 @@
+import { getUserByClerkID } from "@/utils/auth"
+import { prisma } from "@/utils/db"
+
+prisma
+
+const getEntries = async () => {
+    const user = await getUserByClerkID()
+    const entries = await prisma.journalEntry.findMany({
+        where: {
+            userId: user.id,
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+
+    return entries
+}
+
+const JournalPage = async () => {
+    const entries = await getEntries()
+    console.log('entries', entries)
+    return (
+        <div>
+            <h1>Journal</h1>
+        </div>
+    )
+}
+
+export default JournalPage
