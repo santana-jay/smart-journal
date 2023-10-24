bc48e792d6520bb445aea1195486407ed13db1e0
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2023-10-24 19:27:19 -0400

    applies layout and link functionality

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,8 @@
+import EntryCard from "@/components/EntryCard"
+import NewEntryCard from "@/components/NewEntryCard"
 import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
+import Link from "next/link"
 
 prisma
 
@@ -21,8 +24,16 @@ const JournalPage = async () => {
     const entries = await getEntries()
     console.log('entries', entries)
     return (
-        <div>
-            <h1>Journal</h1>
+        <div className="p-10 bg-zinc-400/10 h-full">
+            <h2 className="text-3xl mb-8">Journal</h2>
+            <div className="grid grid-cols-3 gap-4">
+                <NewEntryCard />
+                {entries.map(entry => (
+                    <Link href={`/journal/${entry.id}`} key={entry.id}>
+                        <EntryCard entry={entry} />
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
