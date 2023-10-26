2b3fe5f2a02ee2d5ba4982734670bc9f5860565a
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2023-10-26 15:38:01 -0400

    adds openai functionality

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from "@/components/EntryCard"
 import NewEntryCard from "@/components/NewEntryCard"
+import analyze from "@/utils/ai"
 import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
 import Link from "next/link"
@@ -16,6 +17,21 @@ const getEntries = async () => {
             createdAt: 'desc',
         },
     })
+
+    await analyze(`I'm going to give you a journal entry, I want you to analyze the entry for a few things.
+    I need the mood, a summary, what the subject is, and a color representing the mood.
+    You need to respond  back with a formatted JSON like so:
+    {
+        "mood": "",
+        "summary": "",
+        "subject": "",
+        "color": "",
+        negative: "",
+    }.
+
+    entry:
+    Today was a blah day. I have been looking for a job since graduation and i have had no luck. I am feeling so stressed.
+    `)
 
     return entries
 }
---
+++ utils/ai.ts
@@ -0,0 +1,9 @@
+import { OpenAI } from 'langchain/llms/openai'
+
+const analyze = async (prompt) => {
+    const model = new OpenAI({temperature: 0, modelName: 'gpt-3.5-turbo'})
+    const result = await model.call(prompt)
+    console.log(result)
+}
+
+export default analyze
