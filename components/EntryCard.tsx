9ed04caedfbb25369e1aba2c4c33fa72d3860021
JSantana11b <133810363+JSantana11b@users.noreply.github.com>
2023-11-07 14:05:38 -0500

    testing entry card changes to display summary and mood 2

--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -2,12 +2,13 @@
 
 const EntryCard = ({entry}) => {
     const date = new Date(entry.createdAt).toDateString()
+    const analysis = entry.analysis
 
     return (
         <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
             <div className="px-4 py-5 sm:px-6">{date}</div>
-            <div className="px-4 py-5 sm:px-6">{entry.summary}</div>
-            <div className="px-4 py-5 sm:px-6">{entry.mood}</div>
+            <div className="px-4 py-5 sm:px-6">{analysis.summary}</div>
+            <div className="px-4 py-5 sm:px-6">{analysis.mood}</div>
         </div>
         )
 }
